8df9856f10cb514af68d1699501241e6cf9d6422
Bhupesh-Yedla <bhupesh.yedla@outlook.com>
2017-05-20 14:12:25 +0530

    till payment page

--- TestRunner.java
+++ TestRunner.java
@@ -12,19 +12,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import com.sun.jna.Structure;
 import com.sun.jna.platform.mac.Carbon.EventHotKeyID.ByValue;
 
 public class TestRunner {
 	
 	WebDriver driver;
 	String url;
-	WebElement userID, password, Login, fromStation, toStation, jrnyDate, submit,tatkal,sleeper,booknow,passengerAge, passengerName,passengerGender;
-
+	WebElement nextToPay,passengerBerth,passengerFood,userID, password, Login, fromStation, toStation, jrnyDate, submit,tatkal,sleeper,booknow,passengerAge, passengerName,passengerGender;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -41,12 +42,12 @@ public class TestRunner {
 		driver = new ChromeDriver();
 		url = "http://www.irctc.co.in";
 		driver.get(url);
-		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		//driver.manage().window().maximize();
 	}
 	
 	@Test
-	public void test() throws InterruptedException {
+	public void test() throws Exception {
 		System.out.println("Login");
 		//fail("Not yet implemented");
 		//System.out.println(driver.manage().window().getSize());
@@ -62,7 +63,7 @@ public class TestRunner {
 		toStation = driver.findElement(By.xpath(".//*[@id='jpform:toStation']"));
 		toStation.sendKeys("SECUNDERABAD JN - SC");
 		jrnyDate = driver.findElement(By.xpath(".//*[@id='jpform:journeyDateInputDate']"));
-		jrnyDate.sendKeys("15-05-2017");
+		jrnyDate.sendKeys("21-05-2017");
 		submit = driver.findElement(By.id("jpform:jpsubmit"));
 		submit.click();
 		tatkal = driver.findElement(By.xpath(".//*[@id='qcbd']/table/tbody/tr/td[5]/input"));
@@ -70,22 +71,50 @@ public class TestRunner {
 		System.out.println("selecting Tatkal");
 		tatkal.click();
 		Thread.sleep(5000);
-		sleeper = driver.findElement(By.xpath(".//*[@id='cllink-17013-SL-2']"));
 		System.out.println("selecting SL");
-		sleeper.click();
+		//.//*[@id='cllink-17013-SL-2']-RAJKOTX
+		//dURAANTO 3A - .//*[@id='cllink-12219-3A-2']
+		sleeper = driver.findElement(By.xpath(".//*[@id='cllink-12219-3A-2']"));
+		try{
+			sleeper.click();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 		Thread.sleep(5000);
-		booknow = driver.findElement(By.xpath(".//*[@id='17013-SL-TQ-0']"));
-		booknow.click();
+		//RAJKOT - .//*[@id='17013-SL-TQ-0']
+		//DURANTO BOOK .//*[@id='12219-3A-TQ-0']
+		System.out.println("Booking Now");
+		//By.xpath(".//*[@id='12219-3A-TQ-0']")
+		//booknow = driver.findElements(By.xpath(".//*[@id='12219-3A-TQ-0']"));
+		int varelesize = driver.findElements(By.xpath(".//*[@id='12219-3A-TQ-0']")).size();		
+		System.out.println(varelesize);
+		try{
+		driver.findElements(By.xpath(".//*[@id='12219-3A-TQ-0']")).get(varelesize-1).click();
+		}
+		catch(Exception e)
+		{
+		System.out.println(e);
+		}		
 		System.out.println("Booking Now");
 		Thread.sleep(5000);
-		passengerName = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0:p1652270205']"));
+		passengerName = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0']//td[@id='addPassengerForm:psdetail:0:j_idt558']//input"));
 		passengerName.sendKeys("Bhupesh Yedla");
 		passengerAge = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0:psgnAge']"));
 		passengerAge.sendKeys("27");
-		passengerGender = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0:psgnGender'])"));
-		Select dropdown = new Select(passengerGender);
-		dropdown.selectByIndex(1);
-		
+		passengerGender = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0:psgnGender']"));
+		Select ddGender = new Select(passengerGender);
+		ddGender.selectByIndex(1);		
+		passengerFood = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0:foodChoice']"));
+		Select ddFood = new Select(passengerFood);
+		ddFood.selectByIndex(1);		
+		passengerBerth = driver.findElement(By.xpath(".//*[@id='addPassengerForm:psdetail:0:berthChoice']"));
+		Select ddBerth = new Select(passengerBerth);
+		ddBerth.selectByIndex(1);
+		Thread.sleep(5000);
+		nextToPay = driver.findElement(By.xpath(".//*[@id='validate']"));
+		nextToPay.click();
 		}
 
 	private By ByValue(String string) {
